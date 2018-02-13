276278525e422de4ac0d8f772f941a6bbb9b47de
camilopineda77 <camilopineda77@hotmail.com>
2018-02-13 01:06:09 -0500

    Agregación de nuevos items en el menu lateral y sus iconos

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
+/* Material components*/
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
-import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
-import SocialNotifications from 'material-ui/svg-icons/social/notifications';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import Avatar from 'material-ui/Avatar';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import Menu from 'material-ui/Menu';
 import IconMenu from 'material-ui/IconMenu';
+import Divider from 'material-ui/Divider';
+
+/* Icons */
+import SocialNotifications from 'material-ui/svg-icons/social/notifications';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+import ActionDashboard from 'material-ui/svg-icons/action/dashboard';
+import ActionBuild from 'material-ui/svg-icons/action/build';
+import ActionDateRange from 'material-ui/svg-icons/action/date-range';
+import CommunicationCall from 'material-ui/svg-icons/communication/call';
+import HardwareKeyboardArrowDown from 'material-ui/svg-icons/hardware/keyboard-arrow-down';
+import HardwareKeyboardArrowUp from 'material-ui/svg-icons/hardware/keyboard-arrow-up';
+import MapsDirectionsWalk from 'material-ui/svg-icons/maps/directions-walk';
 
 class Header extends Component {
   constructor( props ) {
     super( props );
     this.state = {
-      open: false
+      open: false,
+      openSubMenuInsp: true,
+      openSubMenuInsp2: false,
     };
   }
 
   handleToggle = () => this.setState({ open: !this.state.open });
+
+  handleInspSubMenu = () =>
+    this.setState({ openSubMenuInsp: !this.state.openSubMenuInsp });
+
+  handleInsp2SubMenu = () =>
+    this.setState({ openSubMenuInsp2: !this.state.openSubMenuInsp2 });
 
   handleClose = () => this.setState({ open: false });
 
@@ -72,6 +92,46 @@ class Header extends Component {
       </div>
     );
 
+    /* render sub menu when is request to open */
+    const subMenuInsp = (
+      this.state.openSubMenuInsp ?
+      <div style = { styles.subMenu }>
+        <MenuItem
+          onClick = { this.handleClose }
+          rightIcon = { <CommunicationCall/> }
+        >
+          Solicitar
+        </MenuItem>
+        <MenuItem
+          onClick = { this.handleClose }
+          rightIcon = { <ActionDateRange/> }
+        >
+          Agendar
+        </MenuItem>
+      </div>  :
+      <div></div>
+    );
+
+    /* render sub menu when is request to open */
+    const subMenuInsp2 = (
+      this.state.openSubMenuInsp2 ?
+      <div style = { styles.subMenu }>
+        <MenuItem
+          onClick = { this.handleClose }
+          rightIcon = { <CommunicationCall/> }
+        >
+          Registrar
+        </MenuItem>
+        <MenuItem
+          onClick = { this.handleClose }
+          rightIcon = { <ActionDateRange/> }
+        >
+          Agendar
+        </MenuItem>
+      </div>  :
+      <div></div>
+    );
+
     return(
       <div style = { styles.header }>
         { /* Navigation Bar */ }
@@ -81,23 +141,65 @@ class Header extends Component {
           iconElementRight = { leftMenu }
           style = { styles.navBar }
         />
-        { /* Menu - Left Navigation Bar */ }
+        { /* --- Left Navigation Bar --- */ }
         <Drawer
           open = { this.state.open }
           docked = { false }
           onRequestChange = { (open) => this.setState({ open }) }
         >
-          <Link to = "/dashboard" >
-            <MenuItem onClick = { this.handleClose }>
-              Dashboard
-            </MenuItem>
-          </Link>
 
-          <Link to = "/inspecciones">
-            <MenuItem onClick = { this.handleClose }>
-            Inspecciones
-            </MenuItem>
-          </Link>
+          <div style = { styles.leftMenuHeader }>
+            <Avatar
+              src = "images/avatar.jpg"
+              style = { styles.avatarLeftMenu }
+            />
+          </div>
+
+          { /*  Menu */ }
+
+            <Link to = "/dashboard" style = { styles.link }>
+              <MenuItem
+                onClick = { this.handleClose }
+                leftIcon = { <ActionDashboard/> }
+              >
+                Dashboard
+              </MenuItem>
+            </Link>
+            <Divider />
+            <Link to = "/inspecciones" style = { styles.link }>
+              <MenuItem
+                onClick = { this.handleInspSubMenu }
+                leftIcon = { <ActionBuild/> }
+                rightIcon = {
+                  this.state.openSubMenuInsp ?
+                    <HardwareKeyboardArrowUp/> :
+                    <HardwareKeyboardArrowDown/>
+                }
+              >
+              Inspecciones
+              </MenuItem>
+            </Link>
+            { /* Sub Menu Inspecciones*/}
+            { subMenuInsp }
+
+            <Divider />
+
+            <Link to = "/inspecciones" style = { styles.link }>
+              <MenuItem
+                onClick = { this.handleInsp2SubMenu }
+                leftIcon = { <MapsDirectionsWalk/> }
+                rightIcon = {
+                  this.state.openSubMenuInsp2 ?
+                    <HardwareKeyboardArrowUp/> :
+                    <HardwareKeyboardArrowDown/>
+                }
+              >
+              Inspectores
+              </MenuItem>
+            </Link>
+            { /* Sub Menu Inspecctores*/}
+            { subMenuInsp2 }
+            <Divider/>
         </Drawer>
       </div>
     );
@@ -112,6 +214,23 @@ const styles = {
     position: 'fixed',
     top: 0,
     height: 65,
+  },
+  leftMenuHeader: {
+    height: 120,
+    backgroundColor: '#00BCD4',
+    textAlign: 'center',
+  },
+  avatarLeftMenu: {
+    position: 'relative',
+    top: 20,
+    height: 80,
+    width: 80
+  },
+  link: {
+    textDecoration: 'none'
+  },
+  subMenu: {
+    textAlign: 'left'
   }
 }
 
