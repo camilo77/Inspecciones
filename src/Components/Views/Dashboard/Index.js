4af56d887270cd7beffffbe2bd2f10dd05f44baf
camilopineda77 <camilopineda77@hotmail.com>
2018-02-14 19:22:40 -0500

    Add file calendar and import into dashboard

--- src/Components/Views/Dashboard/Index.js
+++ src/Components/Views/Dashboard/Index.js
@@ -4,11 +4,14 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Divider from 'material-ui/Divider';
 import { Link } from 'react-router-dom';
 
+import Calendar from '../../Calendar';
+
 class Index extends Component {
   render(){
     return(
       <div>
         <h1>Agenda</h1>
+        <Calendar/>
         <Divider/>
         <h1>Otras Funciones</h1>
 
